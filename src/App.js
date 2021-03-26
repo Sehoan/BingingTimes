4e05e8acbf5179b1948f4149ffef747da218ab57
Sehoan <sehoanchoi0124@gmail.com>
2021-03-26 20:14:11 +0900

    Added a heading and finished setting up http request to nytimes api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://api.nytimes.com/svc/movies/v2"
+const api_key = "0TYh7SlLme99I2NTyZ0Avv9Yk6i9Wv3t"
+
+const App = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get(url + "/critics/all.json?" + `&api-key=${api_key}`)
+            .then(response => {
+                setData(response.data.results)
+            })
+    }, [])
+
+    return (
+        <div>
+            <h1>bingingTimes finds you the best movie from NYtimes</h1>
+            <ul>
+                {data.map(movie => (
+                    <li key={movie.display_name}>
+                        {movie.display_name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
