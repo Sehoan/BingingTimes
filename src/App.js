9d6f78f3c26476589eecab21562379ae4d387122
Sehoan <sehoanchoi0124@gmail.com>
2021-03-28 21:27:04 +0900

    Requested the list of movies that were picked by the critics and listed them in order with short summary and a link for more detail

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import axios from 'axios'
 
 const url = "https://api.nytimes.com/svc/movies/v2"
 const api_key = "0TYh7SlLme99I2NTyZ0Avv9Yk6i9Wv3t"
+const end_point = "/reviews/picks.json?"
 
 const App = () => {
     const [data, setData] = useState([])
     useEffect(() => {
-        axios.get(url + "/critics/all.json?" + `&api-key=${api_key}`)
+        axios.get(url + end_point + `&api-key=${api_key}`)
             .then(response => {
                 setData(response.data.results)
             })
@@ -16,14 +17,24 @@ const App = () => {
 
     return (
         <div>
-            <h1>bingingTimes finds you the best movie from NYtimes</h1>
-            <ul>
-                {data.map(movie => (
-                    <li key={movie.display_name}>
-                        {movie.display_name}
-                    </li>
-                ))}
-            </ul>
+            <h1 style={{textAlign: "center"}}>BingingTimes finds you the best movie from NYtimes</h1>
+            <ol>
+                {data.map(movie => {
+                    if (movie.display_title.localeCompare("") !== 0) {
+                        return (
+                            <li key={movie.display_title}>
+                                <p>{movie.display_title}</p>
+                                <p>{movie.headline}</p>
+                                <img src={movie.multimedia.src} alt="movie scene" />
+                                <p style={{color: "grey"}}>{movie.summary_short}</p>
+                                For more info about the movie, click 
+                                <a href={movie.link.url}> link</a>
+                            </li>
+                        )
+                    }
+                })}
+
+            </ol>
         </div>
     )
 }
